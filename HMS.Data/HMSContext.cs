be195f3c22a717253777a81b5df80afffd49dea4
zubair farooq <zubair.farooq@hotmail.com>
2019-08-23 16:28:36 +0500

    Identity framework install with signIn and signOUT

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,25 @@
+﻿using HMS.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : IdentityDbContext<HMSUser>
+    {
+        public HMSContext() : base("HMSConnectionString")
+        { }
+        public static HMSContext Create()
+        {
+            return new HMSContext();
+        }
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
