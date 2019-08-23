39d298eda0c3dcb63f72603c14d9782e6fab7ced
zubair farooq <zubair.farooq@hotmail.com>
2019-08-23 15:07:50 +0500

    Initialization and Entites

---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public int AccomodationID { get; set; }
+        public Accomodation Accomodation { get; set; }
+
+        public DateTime FromDate { get; set; }
+        /// <summary>
+        /// No of Stay Night
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
